e76121c847e3c61d28b0e1e5757577b564935072
jingtianfeng <jingtianfeng@hotmail.com>
2022-07-11 09:35:35 -0400

    ADD main.js: control flow

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,33 @@
-console.log(`Bootstrap \t 5.1 \t\t ${!!bootstrap ? "LOADED" : "ERROR"}`);
-console.log(`GSAP \t\t 3.10 \t\t ${!!gsap ? "LOADED" : "ERROR"}`);
-
+// ==========================================================================
+// 1. Background
+// --------------------------------------------------------------------------
 const elemBody = document.body;
 elemBody.style.position = "relative";
-
+// --------------------------------------------------------------------------
 const elemBGContainer = document.createElement("div");
 elemBody.appendChild(elemBGContainer);
-
+// --------------------------------------------------------------------------
 elemBGContainer.style.position = "absolute";
 elemBGContainer.style.inset = "0px";
 elemBGContainer.style.zIndex = "-1000";
 elemBGContainer.style.pointerEvents = "none";
 elemBGContainer.style.userSelect = "none";
-
+// --------------------------------------------------------------------------
 elemBGContainer.tabIndex = "-1";
 elemBGContainer.ariaDisabled = "true";
+// ==========================================================================
+// 2. GSAP Animation
+// --------------------------------------------------------------------------
+if (!!gsap) {
+	console.log(`GSAP \t\t ${gsap.version}`);
+} else {
+	console.log("GSAP \t\t ERROR");
+}
+// ==========================================================================
+// 3. Bootstrap Plugins
+// --------------------------------------------------------------------------
+if (!!bootstrap) {
+	console.log(`Bootstrap \t ${bootstrap.Alert.VERSION}`);
+} else {
+	console.log("Bootstrap \t ERROR");
+}
