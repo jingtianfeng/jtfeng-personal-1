de03f242b9e8f7ac371dbf55988fb8cc3babd511
jingtianfeng <jingtianfeng@hotmail.com>
2022-07-11 06:44:40 -0400

    ADD main.js: start working on bg animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,12 @@
 console.log(`Bootstrap \t 5.1 \t\t ${!!bootstrap ? "LOADED" : "ERROR"}`);
 console.log(`GSAP \t\t 3.10 \t\t ${!!gsap ? "LOADED" : "ERROR"}`);
+
+const elemBody = document.body;
+elemBody.style.position = "relative";
+
+const elemBGContainer = document.createElement("div");
+elemBody.appendChild(elemBGContainer);
+
+elemBGContainer.style.position = "absolute";
+elemBGContainer.style.inset = "0px";
+elemBGContainer.style.zIndex = -1000;
