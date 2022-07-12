239de4b7464e794a21dbb78b5bf1e278e38a5a7a
jingtianfeng <jingtianfeng@hotmail.com>
2022-07-12 13:33:13 -0400

    ADD main.js: stub for background particles

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,6 +16,30 @@ elemBGContainer.style.userSelect = "none";
 // --------------------------------------------------------------------------
 elemBGContainer.tabIndex = "-1";
 elemBGContainer.ariaDisabled = "true";
+// --------------------------------------------------------------------------
+let arrParticle = [];
+const NBR_CANVAS_WIDTH = 7680;
+const NBR_CANVAS_HEIGHT = 25000;
+const NBR_PARTICLE_AMOUNT = 500;
+const NBR_PARTICLE_DIAMETER_MIN = 100;
+const NBR_PARTICLE_DIAMETER_MAX = 250;
+// --------------------------------------------------------------------------
+for (let i = 0; i < NBR_PARTICLE_AMOUNT; i++) {
+	let elemParticle = document.createElement("div");
+	let styleParticle = elemParticle.style;
+	styleParticle.position = "absolute";
+	styleParticle.width = `${NBR_PARTICLE_DIAMETER_MAX - (NBR_PARTICLE_DIAMETER_MAX - NBR_PARTICLE_DIAMETER_MIN)*Math.random()}px`;
+	styleParticle.height = styleParticle.width;
+	styleParticle.border = "4px solid darkgrey";
+	styleParticle.borderRadius = `${NBR_PARTICLE_DIAMETER_MAX / 2}px`;
+	styleParticle.top = `${NBR_CANVAS_HEIGHT * Math.random()}px`;
+	styleParticle.left = `${NBR_CANVAS_WIDTH * Math.random()}px`;
+	arrParticle.push(elemParticle);
+}
+// --------------------------------------------------------------------------
+for (let particle of arrParticle) {
+	elemBGContainer.appendChild(particle);
+}
 // ==========================================================================
 // 2. GSAP Animation
 // --------------------------------------------------------------------------
