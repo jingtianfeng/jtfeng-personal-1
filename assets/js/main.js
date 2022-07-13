526aa5daf0c0e5aecf10eb885fc46d5b8571482a
jingtianfeng <jingtianfeng@hotmail.com>
2022-07-13 12:59:19 -0400

    ADD main.js: STR_PARTICLE_BORDER

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,6 +24,7 @@ const NBR_PARTICLE_AMOUNT = 500;
 const NBR_PARTICLE_DIAMETER_MIN = 100;
 const NBR_PARTICLE_DIAMETER_MAX = 250;
 const NBR_PARTICLE_STEP = 10;
+const STR_PARTICLE_BORDER = "10px solid darkgray";
 // --------------------------------------------------------------------------
 const randomizeStep = (nbrStep) => {
 	return Math.trunc(Math.round(Math.random()*nbrStep)/nbrStep*100)/100;
@@ -34,7 +35,7 @@ for (let i = 0; i < NBR_PARTICLE_AMOUNT; i++) {
 	elemParticle.style.position = "absolute";
 	elemParticle.style.width = `${NBR_PARTICLE_DIAMETER_MAX - (NBR_PARTICLE_DIAMETER_MAX - NBR_PARTICLE_DIAMETER_MIN)*randomizeStep(NBR_PARTICLE_STEP)}px`;
 	elemParticle.style.height = elemParticle.style.width;
-	elemParticle.style.border = "4px solid darkgrey";
+	elemParticle.style.border = STR_PARTICLE_BORDER;
 	elemParticle.style.borderRadius = `${NBR_PARTICLE_DIAMETER_MAX / 2}px`;
 	elemParticle.style.top = `${NBR_CANVAS_HEIGHT * Math.random()}px`;
 	elemParticle.style.left = `${NBR_CANVAS_WIDTH * Math.random()}px`;
