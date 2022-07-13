c2981f6709a574ae25189cf9f6c78bc6cdb86f65
jingtianfeng <jingtianfeng@hotmail.com>
2022-07-13 12:25:01 -0400

    FIX main.js: replace all styleParticle with elemParticle.style

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,14 +26,13 @@ const NBR_PARTICLE_DIAMETER_MAX = 250;
 // --------------------------------------------------------------------------
 for (let i = 0; i < NBR_PARTICLE_AMOUNT; i++) {
 	let elemParticle = document.createElement("div");
-	let styleParticle = elemParticle.style;
-	styleParticle.position = "absolute";
-	styleParticle.width = `${NBR_PARTICLE_DIAMETER_MAX - (NBR_PARTICLE_DIAMETER_MAX - NBR_PARTICLE_DIAMETER_MIN)*Math.random()}px`;
-	styleParticle.height = styleParticle.width;
-	styleParticle.border = "4px solid darkgrey";
-	styleParticle.borderRadius = `${NBR_PARTICLE_DIAMETER_MAX / 2}px`;
-	styleParticle.top = `${NBR_CANVAS_HEIGHT * Math.random()}px`;
-	styleParticle.left = `${NBR_CANVAS_WIDTH * Math.random()}px`;
+	elemParticle.style.position = "absolute";
+	elemParticle.style.width = `${NBR_PARTICLE_DIAMETER_MAX - (NBR_PARTICLE_DIAMETER_MAX - NBR_PARTICLE_DIAMETER_MIN)*Math.random()}px`;
+	elemParticle.style.height = elemParticle.style.width;
+	elemParticle.style.border = "4px solid darkgrey";
+	elemParticle.style.borderRadius = `${NBR_PARTICLE_DIAMETER_MAX / 2}px`;
+	elemParticle.style.top = `${NBR_CANVAS_HEIGHT * Math.random()}px`;
+	elemParticle.style.left = `${NBR_CANVAS_WIDTH * Math.random()}px`;
 	arrParticle.push(elemParticle);
 }
 // --------------------------------------------------------------------------
